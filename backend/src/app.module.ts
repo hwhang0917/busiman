63c8af5ad6481acae024749616f84449793b0632
Heesang Whang <hwhang0917@gmail.com>
2021-04-19 19:42:16 +0900

    :card_file_box: Setup typeorm module & postgres database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,38 @@
+import * as Joi from 'joi';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CommonModule } from './common/common.module';
+
+const isEnvDev = process.env.NODE_ENV === 'dev';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod', 'test').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+        PRIVATE_KEY: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: isEnvDev,
+      logging: isEnvDev,
+      entities: [],
+    }),
+    CommonModule,
+  ],
 })
 export class AppModule {}
