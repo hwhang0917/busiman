f846667c9c9d853a829f1c904b86d79a051c069f
Heesang Whang <hwhang0917@gmail.com>
2021-04-24 01:29:04 +0900

    :label: Update api property

--- backend/src/projects/dto/update-document.dto.ts
+++ backend/src/projects/dto/update-document.dto.ts
@@ -1,3 +1,12 @@
-import { CreateDocumentInput } from './create-document.dto';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 
-export class UpdateDocumentInput extends CreateDocumentInput {}
+export class UpdateDocumentInput {
+  @ApiPropertyOptional()
+  @IsString()
+  readonly title?: string;
+
+  @ApiPropertyOptional()
+  @IsString()
+  readonly documentUrl?: string;
+}
--- backend/src/projects/dto/update-project.dto.ts
+++ backend/src/projects/dto/update-project.dto.ts
@@ -1,3 +1,17 @@
-import { CreateProjectInput } from './create-project.dto';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsEnum, IsNumber, IsString } from 'class-validator';
+import { Status } from '../entities/project.entity';
 
-export class UpdateProjectInput extends CreateProjectInput {}
+export class UpdateProjectInput {
+  @ApiPropertyOptional()
+  @IsString()
+  readonly title?: string;
+
+  @ApiPropertyOptional()
+  @IsNumber()
+  readonly revenue?: number;
+
+  @ApiPropertyOptional({ enum: Status, enumName: 'Status' })
+  @IsEnum(Status)
+  readonly status?: Status;
+}
