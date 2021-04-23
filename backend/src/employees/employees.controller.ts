b628228ceacd972a259c32f3c37a1a2e26843bae
Heesang Whang <hwhang0917@gmail.com>
2021-04-23 22:09:11 +0900

    :lock: Update guards

--- backend/src/employees/employees.controller.ts
+++ backend/src/employees/employees.controller.ts
@@ -7,8 +7,10 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
-import { AuthUser } from 'src/auth/auth-user.decorator';
+import { AuthUser } from 'src/auth/decorators/auth-user.decorator';
 import { Admin } from 'src/auth/decorators/admin.decorator';
+import { Approved } from 'src/auth/decorators/approved.decorator';
+import { Public } from 'src/auth/decorators/public.decorator';
 import { CreateAccountInput } from './dto/create-account.dto';
 import { UpdateAccountInput } from './dto/update-account.dto';
 import { EmployeesService } from './employees.service';
@@ -17,21 +19,25 @@ import { Employee } from './entities/employee.entity';
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
+  @Approved()
   @Get()
   getAllEmployees() {
     return this.employeesService.findAll();
   }
 
+  @Approved()
   @Get(':id')
   getEmployeeById(@Param('id') id: number) {
     return this.employeesService.findById(id);
   }
 
+  @Public()
   @Post()
   createEmployee(@Body() createAccountDto: CreateAccountInput) {
     return this.employeesService.createAccount(createAccountDto);
   }
 
+  @Approved()
   @Put(':id')
   updateEmployee(
     @Param('id') id: number,
